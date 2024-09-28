403b7f22d9105995b8bc8329ae54ec2af8f47111
CHRISTIAN DIAZ <christixn23@gmail.com>
2024-09-28 15:11:00 -0500

    ya se escribio el codigo relacionado con los comentario y tipo de datos.

---
+++ src/Semana3.java
@@ -0,0 +1,31 @@
+public class Semana3 {
+    public static void main (String[] args) {
+        //cualquier cosa
+
+        /* cualquier cosa
+        y otra cosa
+
+         */
+        short numeroEntero = 1;
+        byte numeroEntero2 = 5;
+        int numeroEntero3 = 10;
+        long numeroEntero4 = 100;
+
+        float numeroDecimal1 = 3.14f;
+        double numeroDecimal2 = 3.14;// usen este
+
+        char letra = 'A';
+        String texto = "hablen, pelaos";
+
+        boolean esVerdaderos = true;
+
+
+        //Operador Matemtatico
+        int suma = numeroEntero + 37;
+        System.out.println("el resultado es " + suma);
+
+
+
+
+    }
+}
